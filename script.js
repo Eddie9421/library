e4bc82e9b25188e28259fd8660b92483bc0e8b44
Eddie <Eddie11910@outlook.com>
2025-03-10 22:04:59 -0700

    Implemented addBookToLibrary()

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read)
 {
     this.title = title;
@@ -6,4 +8,19 @@ function Book(title, author, pages, read)
     this.read = read;
     this.infoString = `${title} by ${author}, ${pages} pages, ${read ? "already read" : "not read yet"}`;
     this.info = () => this.infoString;
+    this.id = crypto.randomUUID();
 }
+
+function addBookToLibrary(title, author, pages, read)
+{
+    myLibrary.push(new Book(title, author, pages, read));
+}
+
+addBookToLibrary("Bobby", "Bobby Odenkirk", 234, true);
+addBookToLibrary("Ramen", "joe Mama", 532, false);
+
+for (const book of myLibrary)
+{
+    console.log(book.infoString);
+}
+// crypto.randomUUID()
